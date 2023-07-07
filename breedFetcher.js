2ef63ad790a0a8860a3ea59ce19f9f5f7d6a43d5
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-06 18:33:07 -0700

    edge cases implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,23 @@
 const request = require("request");
+const breed = process.argv[2];
 
-request.get("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
-  if (error) {
-    console.error("Error:", error);
-    return;
+request.get(
+  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+  (error, response, body) => {
+    if (error) {
+      console.error("Error:", error);
+      return;
+    }
+    if (!breed) {
+      console.log("No breed input detected");
+      return;
+    }
+    if (body === []) {
+      console.log("There was an error with your breed input");
+    }
+    const data = JSON.parse(body);
+    const description = data[0].description;
+   
+    console.log(description);
   }
-
-  console.log("Response status code:", response.statusCode);
-  console.log("Response body:", body);
-  console.log("Type of body:", typeof body);
-}) 
+);
